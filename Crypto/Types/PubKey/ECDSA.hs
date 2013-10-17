2f4e52f99beb825885fff7386d5eb2831d77155a
Danny Navarro <j@dannynavarro.net>
2013-10-17 18:26:49 +0200

    Add `Signature`, `KeyPair` and `PublicPoint` to ECDSA

--- Crypto/Types/PubKey/ECDSA.hs
+++ Crypto/Types/PubKey/ECDSA.hs
@@ -7,25 +7,50 @@
 -- Portability : Excellent
 --
 module Crypto.Types.PubKey.ECDSA
-    ( PrivateNumber
-    , PrivateKey(..)
+    ( Signature(..)
+    , PublicPoint
     , PublicKey(..)
+    , PrivateNumber
+    , PrivateKey(..)
+    , KeyPair(..)
+    , toPublicKey
+    , toPrivateKey
     ) where
 
-import qualified Crypto.Types.PubKey.ECC as ECC
+import Crypto.Types.PubKey.ECC
 import Data.Data
+
+-- | ECDSA Public Point, usually embedded in ECDSA Public Key
+type PublicPoint = Point
 
 -- | ECDSA Private Number, usually embedded in ECDSA Private Key
 type PrivateNumber = Integer
 
+-- | Represent a ECDSA signature namely R and S.
+data Signature = Signature
+    { sign_r :: Integer -- ^ ECDSA r
+    , sign_s :: Integer -- ^ ECDSA s
+    } deriving (Show,Read,Eq,Data,Typeable)
+
 -- | ECDSA Private Key
 data PrivateKey = PrivateKey
-    { private_params :: ECC.Curve
-    , private_d      :: PrivateNumber
+    { private_curve :: Curve
+    , private_d     :: PrivateNumber
     } deriving (Show,Read,Eq,Data,Typeable)
 
 -- | ECDSA Public Key
 data PublicKey = PublicKey
-    { public_params :: ECC.Curve
-    , public_q      :: ECC.Point
+    { public_curve :: Curve
+    , public_q     :: PublicPoint
     } deriving (Show,Read,Eq,Data,Typeable)
+
+data KeyPair = KeyPair Curve PublicPoint PrivateNumber
+    deriving (Show,Read,Eq,Data,Typeable)
+
+-- | Public key of a ECDSA Key pair
+toPublicKey :: KeyPair -> PublicKey
+toPublicKey (KeyPair curve pub _) = PublicKey curve pub
+
+-- | Private key of a ECDSA Key pair
+toPrivateKey :: KeyPair -> PrivateKey
+toPrivateKey (KeyPair curve _ priv) = PrivateKey curve priv
